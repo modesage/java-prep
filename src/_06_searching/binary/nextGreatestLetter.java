d965a294a1b444e79d4864de0e56a09a13199919
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-08-30 11:01:46 +0530

    Add Leetcode solution

---
+++ src/_06_searching/binary/nextGreatestLetter.java
@@ -0,0 +1,25 @@
+package _06_searching.binary;
+//leetcode
+//744. Find Smallest Letter Greater Than Target
+public class nextGreatestLetter {
+    public static void main(String[] args) {
+        char[] letter = {'c','f','j'};
+        char target = 'j';
+        char result = solution(letter,target);
+        System.out.println(result);
+    }
+    public static char solution(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target<letters[mid]){
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
