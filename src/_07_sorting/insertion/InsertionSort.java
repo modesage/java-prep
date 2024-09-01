5020b1e39e8269bcca30373957e05d32df81a2e5
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-02 05:21:48 +0530

    Add insertion sort

---
+++ src/_07_sorting/insertion/InsertionSort.java
@@ -0,0 +1,28 @@
+package _07_sorting.insertion;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {0,2,54,-5,-98,-7};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if(arr[j] < arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
