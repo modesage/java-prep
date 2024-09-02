af32690b8e5f10d491c54375eb9eadb5257450fc
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-02 21:34:14 +0530

    Add Leetcode solution

---
+++ src/_07_sorting/cyclic/FirstMissingPositive.java
@@ -0,0 +1,35 @@
+package _07_sorting.cyclic;
+//Amazon
+//leetcode
+//41. First Missing Positive
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] arr = {3,4,-1,1};
+        System.out.println(firstMissingPositive(arr));
+    }
+    public static int firstMissingPositive(int[] nums) {
+        cyclic(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+    static void cyclic(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
