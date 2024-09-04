12e963d7dd1c6255e51fdd2bf6d8597249444ac2
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-04 21:18:45 +0530

    Add recursive fibonacci solution

---
+++ src/_10_recursion/Fibonacci.java
@@ -0,0 +1,22 @@
+package _10_recursion;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int num = 5;
+        int result = fibo(num);
+        System.out.println(result);
+    }
+    static int fibo(int num){
+        if (num < 0) {
+            return -1; // Error case: Fibonacci numbers are not defined for negative indices
+        }
+        if (num == 0) {
+            return 0; // Base case: F(0) = 0
+        }
+        if (num == 1) {
+            return 1; // Base case: F(1) = 1
+        }
+        // Recursive case: F(n) = F(n-1) + F(n-2)
+        return fibo(num - 1) + fibo(num - 2);
+    }
+}
