2c13dc4e2432101f279c5d5f86de84ee50917430
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-05 10:27:51 +0530

    Add bitwise oddEven check

---
+++ src/_12_bitwise/OddEven.java
@@ -0,0 +1,12 @@
+package _12_bitwise;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int n = 56;
+        System.out.println(isOdd(n));
+    }
+    static boolean isOdd(int n){
+        return (n & 1) == 1;
+    }
+
+}
