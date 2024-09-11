f994062ee4990c0813b5d21dbdbeddab727a5a5f
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-11 18:36:30 +0530

    Add leetcode solution

--- src/_15_linkedlist/LL.java
+++ src/_15_linkedlist/LL.java
@@ -147,4 +147,34 @@ public class LL {
             this.next = next;
         }
     }
+
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+    public void duplicates() {
+        Node node = head;
+
+        while (node.next != null) {
+            if (node.value == node.next.value) {
+                node.next = node.next.next;
+                size--;
+            } else {
+                node = node.next;
+            }
+        }
+        tail = node;
+        tail.next = null;
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertLast(1);
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(2);
+        list.insertLast(3);
+        list.insertLast(3);
+        list.display();
+        list.duplicates();
+        list.display();
+    }
+
 }
