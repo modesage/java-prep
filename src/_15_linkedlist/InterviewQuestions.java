1189775fc5c531d7f9c07be2fb878b27feea678f
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-13 12:02:54 +0530

    Add leetcode solutions

--- src/_15_linkedlist/InterviewQuestions.java
+++ src/_15_linkedlist/InterviewQuestions.java
@@ -76,5 +76,42 @@ public class InterviewQuestions {
         return s;
     }
 
+    //https://leetcode.com/problems/happy-number/description/
+    //Google
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+    private int findSquare(int number) {
+        int ans = 0;
+        while (number > 0) {
+            int rem = number % 10 ;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
+    }
+
+    //https://leetcode.com/problems/middle-of-the-linked-list/
+    public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
     
 }
