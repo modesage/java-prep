08a8147320f832f9020dd4fe7d5ace8d1a3a0bf4
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-14 17:16:34 +0530

    Add bubble sort approach for linked list

--- src/_15_linkedlist/LL.java
+++ src/_15_linkedlist/LL.java
@@ -194,6 +194,44 @@ public class LL {
         return ans;
     }
 
+    public void bubbleSort() {
+        bubbleSort(size - 1, 0);
+    }
+
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
+
     public static void main(String[] args) {
         LL first = new LL();
         LL second = new LL();
@@ -207,9 +245,15 @@ public class LL {
         second.insertLast(9);
         second.insertLast(14);
 
-        LL ans = merge(first,second);
+        LL ans = LL.merge(first, second);
         ans.display();
 
+        LL list = new LL();
+        for (int i = 7; i > 0; i--) {
+            list.insertLast(i);
+        }
+        list.display();
+        list.bubbleSort();
+        list.display();
     }
-
 }
