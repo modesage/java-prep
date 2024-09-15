a09aa12c6a337a74aaecf6197d49bc6d0094bc01
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-15 12:55:23 +0530

    Add leetcode solution

--- src/_15_linkedlist/InterviewQuestions.java
+++ src/_15_linkedlist/InterviewQuestions.java
@@ -134,5 +134,42 @@ public class InterviewQuestions {
         }
         return prev;
     }
-    
+
+    // Google, Microsoft, Facebook: https://leetcode.com/problems/reverse-linked-list-ii/
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+
+        // skip the first left-1 nodes
+        ListNode current = head;
+        ListNode prev = null;
+        for (int i = 0; current != null && i < left - 1; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        ListNode last = prev;
+        ListNode newEnd = current;
+
+        // reverse between left and right
+        ListNode next = current.next;
+        for (int i = 0; current != null && i < right - left + 1; i++) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+
+        if (last != null) {
+            last.next = prev;
+        } else {
+            head = prev;
+        }
+
+        newEnd.next = current;
+        return head;
+    }
 }
