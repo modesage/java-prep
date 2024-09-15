e82b2d74587f7bbcdf3f4608ebdceb42df7c6533
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-15 10:36:18 +0530

    Add reversal of linked list

--- src/_15_linkedlist/InterviewQuestions.java
+++ src/_15_linkedlist/InterviewQuestions.java
@@ -113,5 +113,26 @@ public class InterviewQuestions {
         }
         return s;
     }
+
+    // https://leetcode.com/problems/reverse-linked-list/submissions/
+    // google, apple, amazon, microsoft
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
     
 }
--- src/_15_linkedlist/LL.java
+++ src/_15_linkedlist/LL.java
@@ -232,6 +232,40 @@ public class LL {
         }
     }
 
+    // recursion reverse
+    private void reverse(Node node) {
+        if (node == tail) {
+            head = tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+
+    // in place reversal of linked list
+    // google, microsoft, apple, amazon: https://leetcode.com/problems/reverse-linked-list/
+    public void reverse() {
+        if (size < 2) {
+            return;
+        }
+
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        head = prev;
+    }
+
     public static void main(String[] args) {
         LL first = new LL();
         LL second = new LL();
