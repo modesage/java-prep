acff5b7eeb7755a59bc10473481c93a8acb3bd2b
Shaurya Dobhal <shauryadobhal76@gmail.com>
2024-09-15 13:44:29 +0530

    Add leetcode solution reorder list

--- src/_15_linkedlist/InterviewQuestions.java
+++ src/_15_linkedlist/InterviewQuestions.java
@@ -193,5 +193,45 @@ public class InterviewQuestions {
         return head == null || headSecond == null;
     }
 
-    
+    // https://leetcode.com/problems/reorder-list/
+    // Google, Facebook
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = middleNode(head);
+
+        ListNode hs = reverseList(mid);
+        ListNode hf = head;
+
+        // rearrange
+        while (hf != null && hs != null) {
+            ListNode temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+
+        // next of tail to null
+        if (hf != null) {
+            hf.next = null;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
 }
