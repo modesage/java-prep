6b57487aa28d2fc30330b223895068cdaa5ca454
Shaurya Dobhal <shauryadobhal76@gmail.com>
2025-01-21 18:14:54 +0530

    Add recursion files

--- src/_10_recursion/_3_array/LinearSearch.java
+++ src/_10_recursion/_3_array/LinearSearch.java
@@ -6,7 +6,7 @@ public class LinearSearch {
     public static void main(String[] args){
         int[] arr = {5,1,2,5,3,4,5,6,12,41,5};
         ArrayList<Integer> list = new ArrayList<>();
-        System.out.println(multipleElementOccurIndex(arr,0,5,list));
+        System.out.println(multipleElementOccurIndex(arr,0,5));
 //        int result = elementIndex(arr,0,6);
 //        System.out.println(result);
     }
@@ -28,6 +28,21 @@ public class LinearSearch {
             list.add(index);
         }
         return multipleElementOccurIndex(arr,++index,element, list);
+    }
+
+    //not is optimized approach, but can keep as a concept for some problem if req
+    static ArrayList multipleElementOccurIndex(int[] arr, int index, int element){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(index == arr.length){
+            return list;
+        }
+        //this will contain answer for that function call only
+        if(arr[index] == element){
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = multipleElementOccurIndex(arr,++index,element);
+        list.addAll(ansFromBelowCalls);
+        return list;
     }
 }
 
