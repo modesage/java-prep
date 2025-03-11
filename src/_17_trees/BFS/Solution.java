804386bcb1036ec832c56a0877cf84e91c83aadf
Shaurya Dobhal <shauryadobhal76@gmail.com>
2025-03-11 10:50:05 +0530

    Add BFS questions

--- src/_17_trees/BFS/Solution.java
+++ src/_17_trees/BFS/Solution.java
@@ -1,8 +1,5 @@
 package _17_trees.BFS;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 // Definition for a binary tree node.
 class TreeNode {
@@ -12,6 +9,27 @@ class TreeNode {
 
     public TreeNode(int val) {
         this.val = val;
+    }
+}
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
     }
 }
 
@@ -78,6 +96,225 @@ public class Solution {
         return result;
     }
 
+    public TreeNode findSuccessor(TreeNode root, int key){
+        //google
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            TreeNode currentNode = queue.poll();
+            if (currentNode.left != null) {
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.offer(currentNode.right);
+            }
+            if (currentNode.val == key) {
+                break;
+            }
+        }
+        return queue.peek();
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+        //google, amazon, microsoft
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean reverse = false;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i=0; i < levelSize; i++) {
+                if (!reverse) {
+                    TreeNode currentNode = queue.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) {
+                        queue.addLast(currentNode.left);
+                    }
+                    if (currentNode.right != null) {
+                        queue.addLast(currentNode.right);
+                    }
+                } else {
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.right != null) {
+                        queue.addFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null) {
+                        queue.addFirst(currentNode.left);
+                    }
+                }
+            }
+            reverse = !reverse;
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+    public List<List<Integer>> zigzagLevelOrderII(TreeNode root) {
+        //https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            result.add(0, currentLevel);
+        }
+        return result;
+    }
+
+    public Node connect(Node root) {
+        //https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+        //amazon london 2022
+        if (root == null) return null;
+        Node leftMost = root;
+        while(leftMost.left != null){
+            Node current = leftMost;
+            while(current != null){
+                current.left.next = current.right;
+                if(current.next != null){
+                    current.right.next = current.next.left;
+                }
+                current = current.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+
+
+    public List<Integer> rightSideView(TreeNode root) {
+        //https://leetcode.com/problems/binary-tree-right-side-view/description/
+        //amazon, flipkart
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                if(i == levelSize - 1){
+                    result.add(currentNode.val);
+                }
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        //https://leetcode.com/problems/cousins-in-binary-tree/
+        TreeNode xx = findNode(root, x);
+        TreeNode yy = findNode(root, y);
+
+        return (
+                (level(root, xx, 0) == level(root, yy, 0)) && (!isSibling(root, xx, yy))
+        );
+    }
+
+    TreeNode findNode(TreeNode node, int x) {
+        if (node == null) {
+            return null;
+        }
+        if (node.val == x) {
+            return node;
+        }
+        TreeNode n = findNode(node.left, x);
+        if (n != null) {
+            return n;
+        }
+        return findNode(node.right, x);
+    }
+
+    boolean isSibling(TreeNode node, TreeNode x, TreeNode y) {
+        if (node == null) {
+            return false;
+        }
+        return (
+                (node.left == x && node.right == y) || (node.left == y && node.right == x)
+                        || isSibling(node.left, x, y) || isSibling(node.right, x, y)
+        );
+    }
+
+    int level(TreeNode node, TreeNode x, int lev) {
+        if(node == null) {
+            return 0;
+        }
+        if(node == x) {
+            return lev;
+        }
+        int l = level(node.left, x, lev+1);
+        if (l != 0) {
+            return l;
+        }
+        return level(node.right, x, lev+1);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        //https://leetcode.com/problems/symmetric-tree/
+        //google, amazon, apple
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while(!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if(left == null && right == null) {
+                continue;
+            }
+            if(left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
 
 //    public static void main(String[] args) {
 //        TreeNode root = new TreeNode(3);
