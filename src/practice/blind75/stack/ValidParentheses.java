73947f7bdd8dabc5560d4f7e979d2bff4ce6978e
Shaurya Dobhal <shauryadobhal76@gmail.com>
2025-09-16 13:09:25 +0530

    Add stack problem

---
+++ src/practice/blind75/stack/ValidParentheses.java
@@ -0,0 +1,25 @@
+package practice.blind75.stack;
+
+import java.util.Stack;
+
+//https://leetcode.com/problems/valid-parentheses/description/
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
